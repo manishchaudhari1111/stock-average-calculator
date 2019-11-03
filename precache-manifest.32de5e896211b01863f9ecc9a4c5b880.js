8f7601bbd64ff8e62aef2b4cae2e5ad4576c862a
Manish <manishchaudhari1111@gmail.com>
2019-11-03 17:38:22 +0530

    Updates

---
+++ precache-manifest.32de5e896211b01863f9ecc9a4c5b880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c2111a4e8427f43037a106cc2c4481",
+    "url": "/stock-average-calculator/index.html"
+  },
+  {
+    "revision": "d55373b4cf3cb3a629c1",
+    "url": "/stock-average-calculator/static/css/main.94fead84.chunk.css"
+  },
+  {
+    "revision": "c28b055e2800a2acb39e",
+    "url": "/stock-average-calculator/static/js/2.17a9ea9f.chunk.js"
+  },
+  {
+    "revision": "d55373b4cf3cb3a629c1",
+    "url": "/stock-average-calculator/static/js/main.20d04dac.chunk.js"
+  },
+  {
+    "revision": "c15f63e735683c05909d",
+    "url": "/stock-average-calculator/static/js/runtime~main.e872f82f.js"
+  }
+]);
